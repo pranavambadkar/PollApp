72e09f4c443472d92d9c044d39c884b761fa2f06
Pranav Ambadkar <125912673+pranavambadkar@users.noreply.github.com>
2023-06-20 17:30:03 +0530

    Update views.py

--- pollapp/views.py
+++ pollapp/views.py
@@ -1,3 +1,6 @@
+# PollApp using Django Web Framework
+# Developed by Pranav Ambadkar
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
